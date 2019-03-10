a476b04956cf87383c2bc3f63c3ee2f723a73e31
jstevens79 <jstevens79@users.noreply.github.com>
2019-03-10 10:26:40 -0500

    adding framework for editing and removing trains

--- assets/app.js
+++ assets/app.js
@@ -50,10 +50,18 @@ function addTrainObj(name, destination, first, frequency) {
   })
 }
 
-function renderTrainObj(id, trainName, destination, firstTime, frequency) {
+function renderTrainObj(id, trainName, destination, firstTime, frequency, update) {
   var trainTimes = updateTrainArrival(frequency, firstTime, currentTime);
-  var trainObj = $('<div>').addClass('trainRow');
-  trainObj.attr('id', id);
+  var trainObj
+  
+  if (update) {
+    trainObj = $('#' + id);
+    trainObj.empty();
+  } else {
+    trainObj = $('<div>').addClass('trainRow');
+    trainObj.attr('id', id);
+  }
+  
   trainObj.attr('data-frequency', frequency);
   trainObj.attr('data-first-time', firstTime);
   var tName = $('<div>').addClass('trainName').text(trainName);
@@ -61,8 +69,13 @@ function renderTrainObj(id, trainName, destination, firstTime, frequency) {
   var tFrequency = $('<div>').addClass('trainFrequency').text(frequency);
   var nextArrival = $('<div>').addClass('nextArrival').text(trainTimes.nextArrival);
   var minutesAway = $('<div>').addClass('minutesAway').text(trainTimes.minutesAway);
-  trainObj.append(tName, tDestination, tFrequency, nextArrival, minutesAway);
-  $('#trains').append(trainObj)
+  var edit = $('<div>').addClass('editSettings').append('<button class="edit" data-key="' + id + '">Edit</button>');
+  trainObj.append(tName, tDestination, tFrequency, nextArrival, minutesAway, edit);
+
+  if (!update) {
+    $('#trains').append(trainObj);
+  }
+ 
 }
 
 database.ref().on("child_added", function(childSnapshot) {
@@ -71,12 +84,25 @@ database.ref().on("child_added", function(childSnapshot) {
     childSnapshot.val().trainName,
     childSnapshot.val().destination,
     childSnapshot.val().firstTime,
-    childSnapshot.val().frequency
+    childSnapshot.val().frequency,
+    false
   )
 })
 
+database.ref().on("child_removed", function(oldChildSnapshot) {
+  $('#' + oldChildSnapshot.key ).remove();
+})
+
 database.ref().on("child_changed", function(childSnapshot) {
-  console.log(childSnapshot)
+  // re-render the updated row
+  renderTrainObj(
+    childSnapshot.key,
+    childSnapshot.val().trainName,
+    childSnapshot.val().destination,
+    childSnapshot.val().firstTime,
+    childSnapshot.val().frequency,
+    true
+  )
 })
 
 $('#train-add').click(function(e) {
@@ -95,9 +121,6 @@ $('#train-add').click(function(e) {
 })
 
 
-
-
-// need to do this for each of the trains
 function updateTrainArrival(freq, first, currentHour) {
   var firstTimeConverted = moment(first, "HH:mm").subtract(1, "years");
   var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
@@ -107,3 +130,13 @@ function updateTrainArrival(freq, first, currentHour) {
   return {nextArrival: moment(nextTrain).format("hh:mm"), minutesAway: tMinutesTillTrain}
 }
 
+$(document).on('click', '.edit', function() {
+  var myKey = $(this).data('key');
+  var myRecord = database.ref(myKey);
+  
+  myRecord.update({frequency: 5})
+  // to remove...
+  //myRecord.remove()
+
+
+})
