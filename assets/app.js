344901355bd6518fa0452e9749a717821c53f5fc
jstevens79 <jstevens79@users.noreply.github.com>
2019-03-10 23:48:19 -0500

    reworking the train row and form rendering setup

--- assets/app.js
+++ assets/app.js
@@ -37,10 +37,93 @@ function getTime(){
 }
 
 getTime()
-var timeInterval = setInterval(getTime, 500); // to be a little more accurate to the second
+var timeInterval = setInterval(getTime, 500);
+
+function renderForm(key, tName, tDest, tFirst, tFreq) {
+  var nameContainer = $('<div>').addClass('formGroup');
+  var nameInput = $('<input>').attr('id', 'name-' + key);
+  nameInput.attr('placeholder', tName);
+  nameContainer.append('<label>Train Name</label>', nameInput);
+
+  var destContainer = $('<div>').addClass('formGroup');
+  var destInput = $('<input>').attr('id', 'dest-' + key);
+  destInput.attr('placeholder', tDest);
+  destContainer.append('<label>Destination</label>', destInput);
+
+  var firstTimeContainer = $('<div>').addClass('formGroup');
+  var firstInput = $('<input>').attr('id', 'first-' + key);
+  firstInput.attr('placeholder', tFirst);
+  firstTimeContainer.append('<label>First Train Time (hh:mm)</label>', firstInput);
+
+  var freqContainer = $('<div>').addClass('formGroup');
+  var freqInput = $('<input>').attr('id', 'freq-' + key);
+  freqInput.attr('placeholder', tFreq);
+  freqContainer.append('<label>Train Frequency (hh:mm)</label>', freqInput);
+  
+  var submitBttn = $('<button>').attr('data-key', key).addClass('update').text('add');
+  var trainForm = $('<form>').addClass('inputForm');
+
+  trainForm.append(nameContainer, destContainer, firstTimeContainer, freqContainer, submitBttn);
+  return trainForm;
+}
+
+function renderTrainObj(id, trainName, destination, firstTime, frequency) {
+  var trainTimes = updateTrainArrival(frequency, firstTime, currentTime);
+  var trainRow = $('#' + id);
+  trainRow.attr('data-frequency', frequency);
+  trainRow.attr('data-first-time', firstTime);
+  var tName = $('<div>').addClass('trainName').text(trainName);
+  var tDestination = $('<div>').addClass('trainDestination').text(destination);
+  var tFrequency = $('<div>').addClass('trainFrequency').text(frequency);
+  var nextArrival = $('<div>').addClass('nextArrival').text(trainTimes.nextArrival);
+  var minutesAway = $('<div>').addClass('minutesAway').text(trainTimes.minutesAway);
+  var editButton = $('<button>').addClass('edit').attr('data-key', id).html('<i class="far fa-edit"></i>');
+  var deleteButton = $('<button>').addClass('delete').attr('data-key', id).html('<i class="far fa-trash-alt"></i>');
+  var settings = $('<div>').addClass('editSettings').append(editButton, deleteButton);
+  return [tName, tDestination, tFrequency, nextArrival, minutesAway, settings]
+}
+
+///////////////// DATABASE INTERFACING ///////////////// 
+$('#add-train').click(function() {
+  database.ref().push({
+    initiated: false
+  })
+});
 
 
-// here's how to handle the add button
+database.ref().on("child_added", function(childSnapshot) {
+  var trainObj = $('<div>').addClass('trainRow').attr('id', childSnapshot.key);
+  if (childSnapshot.val().initiated) {
+    trainObj.attr('data-frequency', childSnapshot.val().frequency);
+    trainObj.attr('data-first-time', childSnapshot.val().firstTime);
+    trainObj.html(renderTrainObj(
+      childSnapshot.key,
+      childSnapshot.val().trainName,
+      childSnapshot.val().destination,
+      childSnapshot.val().firstTime,
+      childSnapshot.val().frequency
+    ))
+  } else {
+    trainObj.html(renderForm(childSnapshot.key))
+  }
+  
+  $('#trains').append(trainObj);
+})
+
+database.ref().on("child_changed", function(childSnapshot) {
+  $('#' + childSnapshot.key).html(renderTrainObj(
+    childSnapshot.key,
+    childSnapshot.val().trainName,
+    childSnapshot.val().destination,
+    childSnapshot.val().firstTime,
+    childSnapshot.val().frequency
+  ))
+})
+
+database.ref().on("child_removed", function(oldChildSnapshot) {
+  $('#' + oldChildSnapshot.key ).remove();
+})
+
 function addTrainObj(name, destination, first, frequency) {
   database.ref().push({
     trainName: name,
@@ -50,62 +133,17 @@ function addTrainObj(name, destination, first, frequency) {
   })
 }
 
-function renderTrainObj(id, trainName, destination, firstTime, frequency, update) {
-  var trainTimes = updateTrainArrival(frequency, firstTime, currentTime);
-  var trainObj
-  
-  if (update) {
-    trainObj = $('#' + id);
-    trainObj.empty();
-  } else {
-    trainObj = $('<div>').addClass('trainRow');
-    trainObj.attr('id', id);
-  }
-  
-  trainObj.attr('data-frequency', frequency);
-  trainObj.attr('data-first-time', firstTime);
-  var tName = $('<div>').addClass('trainName').text(trainName);
-  var tDestination = $('<div>').addClass('trainDestination').text(destination);
-  var tFrequency = $('<div>').addClass('trainFrequency').text(frequency);
-  var nextArrival = $('<div>').addClass('nextArrival').text(trainTimes.nextArrival);
-  var minutesAway = $('<div>').addClass('minutesAway').text(trainTimes.minutesAway);
-  var editButton = $('<button>').addClass('edit').attr('data-key', id).text('edit');
-  var deleteButton = $('<button>').addClass('delete').attr('data-key', id).text('delete');
-  var settings = $('<div>').addClass('editSettings').append(editButton, deleteButton);
-  trainObj.append(tName, tDestination, tFrequency, nextArrival, minutesAway, settings);
 
-  if (!update) {
-    $('#trains').append(trainObj);
-  }
- 
+function updateTrainArrival(freq, first, currentHour) {
+  var firstTimeConverted = moment(first, "HH:mm").subtract(1, "years");
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  var tRemainder = diffTime % freq;
+  var tMinutesTillTrain = freq - tRemainder;
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  return {nextArrival: moment(nextTrain).format("hh:mm"), minutesAway: tMinutesTillTrain}
 }
 
-database.ref().on("child_added", function(childSnapshot) {
-  renderTrainObj(
-    childSnapshot.key,
-    childSnapshot.val().trainName,
-    childSnapshot.val().destination,
-    childSnapshot.val().firstTime,
-    childSnapshot.val().frequency,
-    false
-  )
-})
-
-database.ref().on("child_removed", function(oldChildSnapshot) {
-  $('#' + oldChildSnapshot.key ).remove();
-})
-
-database.ref().on("child_changed", function(childSnapshot) {
-  renderTrainObj(
-    childSnapshot.key,
-    childSnapshot.val().trainName,
-    childSnapshot.val().destination,
-    childSnapshot.val().firstTime,
-    childSnapshot.val().frequency,
-    true
-  )
-})
-
+// Handle additions, edits, and deletions
 $('#train-add').click(function(e) {
   e.preventDefault()
   var trainName = $('#train-name').val().trim();
@@ -121,28 +159,18 @@ $('#train-add').click(function(e) {
 
 })
 
-
-function updateTrainArrival(freq, first, currentHour) {
-  var firstTimeConverted = moment(first, "HH:mm").subtract(1, "years");
-  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-  var tRemainder = diffTime % freq;
-  var tMinutesTillTrain = freq - tRemainder;
-  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-  return {nextArrival: moment(nextTrain).format("hh:mm"), minutesAway: tMinutesTillTrain}
-}
-
 $(document).on('click', '.edit', function() {
   var myKey = $(this).data('key');
   
   database.ref(myKey).once('value').then(function(snapshot) {
-    var nameForm = $('<input>').attr('id', 'updateName').attr('placeholder', snapshot.val().trainName);
-    var destForm = $('<input>').attr('id', 'updateDest').attr('placeholder', snapshot.val().destination);
-    var startForm = $('<input>').attr('id', 'updateStart').attr('placeholder', snapshot.val().firstTime);
-    var freqForm = $('<input>').attr('id', 'updateFreq').attr('placeholder', snapshot.val().frequency);
-    var update = $('<button>').addClass('update').attr('data-key', myKey).text('Update');
-    $('#' + myKey).empty().append(nameForm, destForm, startForm, freqForm, update);  
+    $('#' + myKey).empty().html(renderForm(
+      myKey,
+      snapshot.val().trainName,
+      snapshot.val().destination,
+      snapshot.val().firstTime,
+      snapshot.val().frequency)
+    );
   });
-
 })
 
 $(document).on('click', '.delete', function() {
@@ -150,26 +178,30 @@ $(document).on('click', '.delete', function() {
   database.ref(myKey).remove();
 })
 
-$(document).on('click', '.update', function() {
+
+
+$(document).on('click', '.update', function(e) {
+  e.preventDefault();
   var myKey = $(this).data('key');
-  var valsToUpdate = {};
+  var valsToUpdate = {initiated: true};
 
-  if ($('#updateName').val() !== '') {
-    valsToUpdate.trainName = $('#updateName').val().trim()
+  if ($('#name-' + myKey).val() !== '') {
+    valsToUpdate.trainName = $('#name-' + myKey).val().trim()
   }
 
-  if ($('#updateDest').val() !== '') {
-    valsToUpdate.destination = $('#updateDest').val().trim()
+  if ($('#dest-' + myKey).val() !== '') {
+    valsToUpdate.destination = $('#dest-' + myKey).val().trim()
   }
 
-  if ($('#updateStart').val() !== '') {
-    valsToUpdate.firstTime = $('#updateStart').val().trim()
+  if ($('#first-' + myKey).val() !== '') {
+    valsToUpdate.firstTime = $('#first-' + myKey).val().trim()
   }
 
-  if ($('#updateFreq').val() !== '') {
-    valsToUpdate.frequency = $('#updateFreq').val().trim()
+  if ($('#freq-' + myKey).val() !== '') {
+    valsToUpdate.frequency = $('#freq-' + myKey).val().trim()
   }
 
   database.ref(myKey).update(valsToUpdate);
+  // what if there are no changes?
   
 })
